5b19eb0166923b3dc12ee388bbecd40d9a8f760a
daniel john buco <djbuuuco@gmail.com>
2024-05-12 15:39:02 +0800

    new

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
 
 const ProfilePage = () => {
   const [watchlists, setWatchlists] = useState([
@@ -47,6 +48,50 @@ const ProfilePage = () => {
   return (
     <div className="profile-page">
       <div className="profile-info">
+      <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol md="12" xl="4">
+            <MDBCard style={{ borderRadius: '15px' }}>
+              <MDBCardBody className="text-center">
+                <div className="mt-3 mb-4">
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava2-bg.webp"
+                    className="rounded-circle" fluid style={{ width: '100px' }} />
+                </div>
+                <MDBTypography tag="h4">Julie L. Arsenault</MDBTypography>
+                <MDBCardText className="text-muted mb-4">
+                  @Programmer <span className="mx-2">|</span> <a href="#!">mdbootstrap.com</a>
+                </MDBCardText>
+                <div className="mb-4 pb-2">
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="facebook" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating className="mx-1">
+                    <MDBIcon fab icon="twitter" size="lg" />
+                  </MDBBtn>
+                  <MDBBtn outline floating>
+                    <MDBIcon fab icon="skype" size="lg" />
+                  </MDBBtn>
+                </div>
+                <MDBBtn rounded size="lg">
+                  Message now
+                </MDBBtn>
+                <div className="d-flex justify-content-between text-center mt-5 mb-2">
+                  <div>
+                    <MDBCardText className="mb-1 h5">8471</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Wallets Balance</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">8512</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">4751</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Total Transactions</MDBCardText>
+                  </div>
+                </div>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
         <h2 className="username">backburner25!</h2>
         <p className="bio">Nagmahal nang wagas sa wala namang label.</p>
         <button className="edit-bio-button">Edit</button>
